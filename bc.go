e737f841f141a1267c906bd3f5cd55292bd94cb4
Leo Flores <leodflowers@gmail.com>
2021-04-02 12:39:26 -0400

    constructors set up

--- bc.go
+++ bc.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
-	"errors"
+	"crypto/sha256"
+	"bytes"
+	"encoding/binary"
 )
 
 type Transaction struct {
@@ -18,44 +21,61 @@ type Block struct {
 	timestamp	time.Time
 	transactions	[]Transaction
 	proof		int
-	previous_hash	string
+	previous_hash	[]byte
 }
 
 
 type Blockchain struct {
 	transactions	[]Transaction
-	chain		[]Block
+	chain		[]*Block
 }
 
 
+// creates new blockchain with a genesis block
 func new_blockchain() *Blockchain {
 	new_bc := new(Blockchain)
 	new_bc.transactions = make([]Transaction, 0)
-	new_bc.chain = make([]Block, 0)
+	new_bc.chain = make([]*Block, 0)
 
-	new_bc.chain = append(new_bc.chain, new_bc.new_block(1, "100"))	// genesis block
+	new_bc.chain = append(new_bc.chain, new_bc.new_block(1, []byte("100")))	// genesis block
 	return new_bc
 }
 
-
-func (bc Blockchain) new_block(pf int, prev_hash string) Block {
-	block := Block{
-		index: len(bc.chain) + 1,
-		timestamp: time.Now(),
-		transactions: bc.transactions,
-		proof: pf,
-		previous_hash: prev_hash,
-	}
+// creates a single block and adds it to chain
+func (bc Blockchain) new_block(pf int, prev_hash []byte) *Block {
+	block := &Block{index: len(bc.chain) + 1,
+			timestamp: time.Now(),
+			transactions: bc.transactions,
+			proof: pf,
+			previous_hash: prev_hash,
+		}
 
 	bc.transactions = nil
+	bc.chain = append(bc.chain, block)
 
 	return block
 }
 
 
+// adds new transaction to list of currently floating transactions
 func (bc Blockchain) new_transaction(amt int, s string, r string) int {
 	bc.transactions = append(bc.transactions, Transaction{amt, s, r})
 	return len(bc.chain) + 1
 }
+
+// hashes block and returns its hash
+func hash_block(b Block) []byte {
+	buffer := new(bytes.Buffer)
+
+	err := binary.Write(buffer, binary.BigEndian, b)
+	if err != nil {
+		log.Fatal("encode error: ", err)
+	}
+
+	h := sha256.New()
+	h.Write(buffer.Bytes())
+	return h.Sum(nil)
+}
+
 
 
